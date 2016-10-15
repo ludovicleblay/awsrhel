8ba1f4163b4e54814a89a7ffe4918aa2bac42381
ludovicleblay <l.leblay@gmail.com>
2016-10-15 19:05:18 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+sudo su
+yum update
+curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
+yum -y install nodejs
+curl -fsSL https://get.docker.com/ | sh
+systemctl enable docker.service
+systemctl start docker
+systemctl enable docker
