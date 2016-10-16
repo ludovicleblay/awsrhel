47119cbb3bdae977fbf34b3e35496728c3d145dc
ludovicleblay <l.leblay@gmail.com>
2016-10-16 08:31:50 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo su
-yum update
+yum -y update
 curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
-yum install -y gcc-c++ make
+yum -y install gcc-c++ make
 yum -y install nodejs
 curl -fsSL https://get.docker.com/ | sh
 systemctl enable docker.service
