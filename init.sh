32061c60e59e6e9b405487ec4eafbe450cd7e666
ludovicleblay <l.leblay@gmail.com>
2016-10-16 08:24:30 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,3 +7,7 @@ curl -fsSL https://get.docker.com/ | sh
 systemctl enable docker.service
 systemctl start docker
 systemctl enable docker
+rpm -Uvh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
+yum -y install openconnect vpnc
+exit
+
