7c60bfbc425288e8e09c6cd87e2c7f183bbb3a03
ludovicleblay <l.leblay@gmail.com>
2016-10-17 15:11:33 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,10 @@ curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 yum -y install gcc-c++ make
 yum -y install nodejs
 yum -y install git
+yum -y install wget
+wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+yum install -y apache-maven
 npm install -g grunt-cli
 npm install -g pm2
 npm config set registry http://10.21.182.123:4873/
